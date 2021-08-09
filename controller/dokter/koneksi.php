63d212c3d78cfbb7c912c7b25296ddf4cc04e290
iam-heisenberg <alitputra05@gmail.com>
2021-08-09 18:21:56 +0800

    koneksi edit

--- controller/dokter/koneksi.php
+++ controller/dokter/koneksi.php
@@ -1,5 +1,14 @@
 <?php
-	$koneksi = mysqli_connect("localhost","root","","db_riwayat_pasien"); 
+
+	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$cleardb_server = $cleardb_url["host"];
+	$cleardb_username = $cleardb_url["user"];
+	$cleardb_password = $cleardb_url["pass"];
+	$cleardb_db = substr($cleardb_url["path"],1);
+	$active_group = 'default';
+	$query_builder = TRUE;
+
+	$koneksi = mysqli_connect("$cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db"); 
 	if (mysqli_connect_errno()) {
 		echo "Koneksi database eror : ". mysqli_connect_error();
 	}
