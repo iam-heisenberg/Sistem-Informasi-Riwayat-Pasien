a7ce6934f23a1eff097b78b84fde06f326ddb755
iam-heisenberg <88607795+iam-heisenberg@users.noreply.github.com>
2021-08-09 21:18:31 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -3,8 +3,5 @@ $hostname='us-cdbr-east-04.cleardb.com';
 $username='b06e4d78cc70c5';
 $password='e92d0a86';
 $dbname='heroku_61847ab65c6aed4';
-
-
-mysql_connect($hostname,$username, $password) OR DIE ('Unable to connect to database! Please try again later.');
-mysql_select_db($dbname);
+$koneksi=mysqli_connect($hostname,$username, $password, $dbname);
 ?>
