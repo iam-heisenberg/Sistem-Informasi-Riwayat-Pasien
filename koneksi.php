5cb17b71cbd0164fbd85ed3bf1d32cfb0f72abea
iam-heisenberg <88607795+iam-heisenberg@users.noreply.github.com>
2021-08-09 21:11:44 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,21 +1,10 @@
 <?php
-	$host = 'us-cdbr-east-04.cleardb.com'; 
-	$db = 'e92d0a86';
-	$user = 'b06e4d78cc70c5'; 
-	$pass = 'e92d0a86';
-	$charset = 'utf8mb4';
+$hostname='us-cdbr-east-04.cleardb.com';
+$username='b06e4d78cc70c5';
+$password='e92d0a86';
+$dbname='heroku_61847ab65c6aed4';
 
-	// $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
-	// try{
-	// 	$pdo = new PDO($dsn, $user, $pass); 
-	// 	$pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-	// } catch (PDOException $e){
-	// 	throw new PDOException($e->getMessage());
-	// }
-
-	$koneksi = mysqli_connect("remotemysql.com,crbjmgeTtQ,FeK6dpleJ2,crbjmgeTtQ"); 
-	if (mysqli_connect_errno()) {
-		echo "Koneksi database eror asdasdasdasdasd: ". mysqli_connect_error();
-	}
+mysql_connect($hostname,$username, $password) OR DIE ('Unable to connect to database! Please try again later.');
+mysql_select_db($dbname);
 ?>
