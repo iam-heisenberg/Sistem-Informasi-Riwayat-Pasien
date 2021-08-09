50e3ea5efcf6b448b0316d4c523ccb4de06f2c0f
iam-heisenberg <alitputra05@gmail.com>
2021-08-09 23:51:54 +0800

    delete ini saja

--- controller/dokter/koneksi.php
+++ controller/dokter/koneksi.php
@@ -5,7 +5,4 @@
 	$password='e92d0a86';
 	$dbname='heroku_61847ab65c6aed4';
 	$koneksi=mysqli_connect($hostname,$username, $password, $dbname);
-
-    echo "tes satudua tiga"; 
-    echo "kamu ini mau nya jadi apa sih sebenenrnya";
 ?>
