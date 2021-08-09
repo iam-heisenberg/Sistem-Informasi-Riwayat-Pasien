cce99f8cda2fa6d2bb27c4663ed4bb594e52ab9f
iam-heisenberg <88607795+iam-heisenberg@users.noreply.github.com>
2021-08-09 20:22:26 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
-	$koneksi = mysqli_connect("us-cdbr-east-04.cleardb.com,b06e4d78cc70c5,e92d0a86,heroku_61847ab65c6aed4"); 
+	$koneksi = mysqli_connect("www.cleardb.com,b06e4d78cc70c5,e92d0a86,heroku_61847ab65c6aed4"); 
 	if (mysqli_connect_errno()) {
-		echo "Koneksi database  sdfdsfdsfdsfdsfsdf erorjbjkbjkbjk : ". mysqli_connect_error();
+		echo "Koneksi database   hello  sdfdsfdsfdsfdsfsdf erorjbjkbjkbjk : ". mysqli_connect_error();
 	}
 ?>
